13f7ebba3639ab4a161fcf7787111ac094f41aa7
Caique Bispo Ferreira <engineer.caique@gmail.com>
2020-07-14 16:30:31 -0400

    Initial commit

---
+++ main/java/com/springdatajpa/SpringDataJpa.java
@@ -0,0 +1,12 @@
+package com.springdatajpa;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+@SpringBootApplication
+public class SpringDataJpa extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpa.class, args);
+	}
+
+}
